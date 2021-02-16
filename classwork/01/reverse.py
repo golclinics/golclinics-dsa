1653c99013eccc37def91edc4d0ec063f7fdb0e0
lennykioko <lennykmutua@gmail.com>
2021-02-16 11:01:45 +0300

    Implement iterative solution

--- classwork/01/reverse.py
+++ classwork/01/reverse.py
@@ -3,10 +3,14 @@ def reverse(array):
     Returns the reverse of the given array
     O(n) TS
     """
+    rev_array = []
     if len(array) == 0:
         return array
     else:
-        return array[::-1]
+        for i in range(len(array)-1, -1, -1):
+            rev_array.append(array[i])
+
+        return rev_array
 
 
 array = [1, 2, 3, 4, 5, 6, 7]
