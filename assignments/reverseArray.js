8a9be4ec19dca2748d1fdae5fce3c06f34c0a2de
dijiflex <dijiflex@gmail.com>
2021-02-16 08:40:53 +0300

    warm up challenge 1

---
+++ assignments/reverseArray.js
@@ -0,0 +1,17 @@
+
+
+function reverse(A){
+    let output = [];
+
+    //recursive function to reverse an array
+    function reverseArray (arr){
+      if (arr.length !== 0){
+        output.push( arr.pop() );
+        reverseArray( arr );
+      }
+    }
+  
+    reverseArray(A);
+    return output;
+}
+reverse([1,2,5,6]) /// [ 6, 5, 2, 1 ]  O(n)
