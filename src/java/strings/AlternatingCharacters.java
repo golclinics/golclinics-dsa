c4f19889b94e28f16ccfc37b906283765d1d7b8c
Sammy Mutahi <sammymutahi12@gmail.com>
2021-02-17 22:10:45 +0300

    alternating characters string solution

--- src/java/strings/AlternatingCharacters.java
+++ src/java/strings/AlternatingCharacters.java
@@ -4,9 +4,7 @@ public class AlternatingCharacters {
     static int alternatingCharacters(String s) {
         int count = 0;
         for(int i=0;i<s.length()-1;i++){
-            if(s.charAt(i) != s.charAt(i+1)){
-                continue;
-            }else{
+            if (s.charAt(i) == s.charAt(i + 1)) {
                 count++;
             }
         }
