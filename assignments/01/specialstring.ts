e1746ac1ae79dc9adceadba444c07dacfe0f9cf9
eduuh <edwinkamaumuraya0@outlook.com>
2021-02-23 07:25:14 +0300

    feat: implemented special string

---
+++ assignments/01/specialstring.ts
@@ -0,0 +1,44 @@
+function substrCount(n, s) {
+      let history = [];
+    let historyCount = 0;
+
+
+    // historyItem = [current, occurrence]
+    let historyItem = [s[0], 1];
+
+    let palindromicCount = 0;
+
+    for(let i = 1; i < n; i++ )
+        if(s[i] == historyItem[0])
+            historyItem[1]++;
+        else{
+            // Calcuate all strings that can be formed with the same character
+            // Count total substrings  -> n * (n + 1) / 2 for
+            palindromicCount += historyItem[1] * (historyItem[1] + 1) / 2;
+
+
+            if(historyCount == 2){
+                // Calcuate all strings that can be formed with the same character except middle character
+                if(history[0][0] == historyItem[0] && history[1][1] == 1 )
+                    palindromicCount += Math.min(history[0][1], historyItem[1])
+
+                history.shift();
+                historyCount--;
+            }
+
+            history.push(historyItem);
+            historyCount++;
+            historyItem = [s[i], 1];
+        }
+
+    palindromicCount += historyItem[1] * (historyItem[1] + 1) / 2;
+
+    if(historyCount == 2)
+        if(history[0][0] == historyItem[0] && history[1][1] == 1 )
+                    palindromicCount += Math.min(history[0][1], historyItem[1])
+
+
+    return palindromicCount;
+
+
+}
