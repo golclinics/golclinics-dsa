45425b4b2da118c37acdd24eb5d87074819c59c2
eduuh <edwinkamaumuraya0@outlook.com>
2021-02-25 20:37:13 +0300

    Feat: not figured this out fast

---
+++ assignments/01/arraymanipulation.ts
@@ -0,0 +1,19 @@
+function arrayManipulation(n, queries) {
+  let arr = [];
+  let max = 0;
+
+  // n is the size of array > defined by  n and queries.length
+  for (let l = 0; l < n; l++) {
+    arr[l] = 0;
+  }
+  // TODO > figure this out
+  return max;
+}
+
+let queries = [
+  [1, 5, 3],
+  [4, 8, 7],
+  [6, 9, 1],
+];
+
+console.log(arrayManipulation(10, queries));
--- assignments/01/specialstring.ts
+++ assignments/01/specialstring.ts
@@ -1,44 +1,50 @@
 function substrCount(n, s) {
-      let history = [];
-    let historyCount = 0;
+  let history = [];
+  let historyCount = 0;
 
+  // historyItem = [current, occurrence]
+  // if seen again the counter is incremented
+  let seentimes = [s[0], 1];
 
-    // historyItem = [current, occurrence]
-    let historyItem = [s[0], 1];
+  let pcount = 0;
 
-    let palindromicCount = 0;
+  for (let i = 1; i < n; i++)
+    if (s[i] == seentimes[0]) seentimes[1]++;
+    else {
+      // Calcuate all strings that can be formed with the same character
+      // Count total substrings  -> n * (n + 1) / 2 for
+      pcount += (seentimes[1] * (seentimes[1] + 1)) / 2;
 
-    for(let i = 1; i < n; i++ )
-        if(s[i] == historyItem[0])
-            historyItem[1]++;
-        else{
-            // Calcuate all strings that can be formed with the same character
-            // Count total substrings  -> n * (n + 1) / 2 for
-            palindromicCount += historyItem[1] * (historyItem[1] + 1) / 2;
+      if (historyCount == 2) {
+        // Calcuate all strings that can be formed with the same character except middle character
+        if (history[0][0] == seentimes[0] && history[1][1] == 1)
+          pcount += Math.min(history[0][1], seentimes[1]);
+        history.shift(); // remove the first element. not good O(n). but the size is one
+        historyCount--; // decrement
+      }
 
+      history.push(seentimes); // O(n)
+      historyCount++;
+      seentimes = [s[i], 1];
+    }
 
-            if(historyCount == 2){
-                // Calcuate all strings that can be formed with the same character except middle character
-                if(history[0][0] == historyItem[0] && history[1][1] == 1 )
-                    palindromicCount += Math.min(history[0][1], historyItem[1])
+  pcount += (seentimes[1] * (seentimes[1] + 1)) / 2;
 
-                history.shift();
-                historyCount--;
-            }
+  if (historyCount == 2)
+    if (history[0][0] == seentimes[0] && history[1][1] == 1)
+      pcount += Math.min(history[0][1], seentimes[1]);
 
-            history.push(historyItem);
-            historyCount++;
-            historyItem = [s[i], 1];
-        }
-
-    palindromicCount += historyItem[1] * (historyItem[1] + 1) / 2;
-
-    if(historyCount == 2)
-        if(history[0][0] == historyItem[0] && history[1][1] == 1 )
-                    palindromicCount += Math.min(history[0][1], historyItem[1])
-
-
-    return palindromicCount;
-
-
+  return pcount;
 }
+
+// test 1
+let n = 1;
+let s = "aaa";
+
+// test 1
+let n1 = 6;
+let s2 = "aaaaabb";
+
+console.log(substrCount(n, s));
+
+console.log(substrCount(n1, s2));
