b33ffba47539411e831b26d4af4a5ecf1e866b6a
mutugiii <mutugimutuma@gmail.com>
2021-05-20 11:02:03 +0300

    Push old changes

---
+++ classwork/03/warm_up.py
@@ -0,0 +1,15 @@
+def evaluate(S):
+    stack = []
+    for val in S:
+        if val != " ":
+            stack.append(value)
+
+    i = 0
+    while i < len(S):
+        if stack[i] == "+":
+            i += 1 
+        else if stack[i] == "-":
+            i += 1
+        else:
+            i += 1
+
