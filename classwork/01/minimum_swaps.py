84abae1e05dd064b2ac2f25a70e238210fe6edc6
ero <eric.ndira@gmail.com>
2021-07-03 21:34:50 +0300

    minimum swaps problem optimised on time using hashmap

--- classwork/01/minimum_swaps.py
+++ classwork/01/minimum_swaps.py
@@ -1,16 +1,16 @@
+# optimized on time to O(N)
 def minimumSwaps(arr):
-    # create a dictionary which holds value index pairs of our array
+    index_value = dict(enumerate(arr, 1))
+    value_index = {v: k for k, v in index_value.items()}
 
-    swaps = 0
-
-    for i in range(len(arr)):
-        pos = i+1
-
-        while pos != arr[i]:
-            val = arr[i]
-            arr[i], arr[val - 1] = arr[val - 1], arr[i]
-            swaps += 1
-
-    return swaps
+    count = 0
+    for pos in index_value:
+        value = index_value[pos]
+        if value != pos:
+            index = value_index[pos]
+            index_value[index] = value
+            value_index[value] = index
+            count += 1
+    return count
 
 print(minimumSwaps([1,3,5,2,4,6,7]))
