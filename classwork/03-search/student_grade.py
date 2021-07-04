017497696fb724122c69e555721620d539489b19
ero <eric.ndira@gmail.com>
2021-07-04 23:36:51 +0300

    solved grade student problem using recursion and binary search

--- classwork/03-search/student_grade.py
+++ classwork/03-search/student_grade.py
@@ -53,4 +53,35 @@ def super_students(grades, students):
 
     return ab_students
 
+# binary search
+def grade_student_bs(marks, grades):
+    l = 0
+    r = len(grades)
+
+    mid = l + (r-l)//2
+    if grades[mid].start <= marks and grades[mid].to >= marks:
+        return grades[mid].gradeName
+    elif grades[mid].start < marks:
+        r = mid
+        return grade_student_bs(marks, grades[l:r])
+    elif grades[mid].to > marks:
+        l = mid + 1
+        return grade_student_bs(marks, grades[l:r])
+
+
+def super_students_bs(grades, students):
+
+    ab_students = []
+
+    for student in students:
+        
+        grade = grade_student_bs(student.marks, grades)
+
+        if grade == 'A' or grade == 'B':
+            ab_students.append(student.name)
+
+    return ab_students
+
 print(super_students(grades, students))
+# binary search recursive
+print(super_students_bs(grades, students))
